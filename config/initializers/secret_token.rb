288283aca4d348646396354207cb139e1a1ec612
Ryan <asheepingreen@hotmail.com>
2011-07-16 15:23:47 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Avouch::Application.config.secret_token = '045e0d98fab85f859503566044f8b9afe8bedf0b69b2537e682357c88dca8271a766acfe17db9f8863f7b77390fabe83e78d62c1a9e0fcaa9a6d662bdc850bd5'
