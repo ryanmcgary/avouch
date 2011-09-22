5a38669d1c5d4550a26f3341a880a30f568fd46f
Ryan McGary <asheepingreen@hotmail.com>
2011-09-22 10:11:36 -0500

    messing with css

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,18 +2,19 @@
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function() {
 
- var profiles = {window800:{height:400,width:500,center:1,scrollbars:0,status:1,menubar:0,toolbars:0,location:0,status:0,resizable:0,onUnload:unloadcallback}};
+ var profiles = {window800:{height:400,width:500,center:1,scrollbars:0,menubar:0,toolbars:0,location:0,status:0,resizable:0,onUnload:unloadcallback}};
  $( function() {
 	 $(".auth_popup").popupwindow(profiles); 
  });
+
  function unloadcallback(){
 	location.reload();
- 	document.getElementById('coolframe').contentWindow.history.back(-1);
+	document.getElementById('coolframe').contentWindow.history.back(-1);
  };
 
  $('.short-text').click(function() {
- 	$('.expand-bottom').slideDown(0.1);
- 	return false;
+	$('.expand-bottom').slideDown(0.1);
+	return false;
  });
 
 });
