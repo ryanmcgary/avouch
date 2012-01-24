92f8a1c91e11e418770f600be1b50f47500a5ed4
Ryan McGary <asheepingreen@hotmail.com>
2012-01-24 16:14:59 -0600

    changed everything from reading localhost:3000 to URL db and devise issues

--- app/controllers/recordings_controller.rb
+++ app/controllers/recordings_controller.rb
@@ -8,8 +8,8 @@ ACCOUNT_TOKEN = '64e5c64955c6b9e1d68f0a9f58b0944b'
 # version of the Twilio REST API to use
 API_VERSION = '2010-04-01'
 
-# base URL of this application
-BASE_URL = "http://stormy-winter-617.heroku.com/recordings"
+# base URL of this application TODO: really need to change this controller to use proper rails routes instead of this BASE_URL nonsense
+BASE_URL = root_url + "recordings"
 # BASE_URL = "http://localhost:3000/recordings"  
 
 # Outgoing Caller ID you have previously validated with Twilio
