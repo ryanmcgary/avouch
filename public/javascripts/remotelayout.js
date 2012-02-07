ce3cbcfc08f848d45613b19d406a9b443ce064c0
Ryan McGary <asheepingreen@hotmail.com>
2012-02-07 16:18:54 -0600

    css for alerts .close, layouts js includes, up jquery to 1.7.1

---
+++ public/javascripts/remotelayout.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+ var profiles = {window800:{height:400,width:500,center:1,scrollbars:0,menubar:0,toolbars:0,location:0,status:0,resizable:0,onUnload:unloadcallback}};
+ $( function() {
+	 $(".auth_popup").popupwindow(profiles); 
+ });
+ // callback for above function
+ function unloadcallback(){
+	location.reload();
+	document.getElementById('coolframe').contentWindow.history.back(-1);
+ };
+
+ $('.short-text').click(function() {
+	$('.expand-bottom').slideDown(0.1);
+	return false;
+ });
+ 
+ // $(".alert-message").alert()
+
+});
